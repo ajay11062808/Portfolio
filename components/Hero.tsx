0d440c0c8b2b156541934c5434a7c1956a2a99a0
Ajay <60562643+ajay11062808@users.noreply.github.com>
2025-01-09 18:58:48 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,11 +2,12 @@ export default function Hero() {
     return (
       <section id="home" className="min-h-screen flex items-center justify-center py-20 bg-gray-100 dark:bg-gray-900">
         <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4 text-gray-800 dark:text-white">Your Name</h1>
+          <h1 className="text-4xl font-bold mb-4 text-gray-800 dark:text-white">Ajay Jagarlamudi</h1>
           <h2 className="text-2xl mb-6 text-gray-800 dark:text-white">Full Stack Developer</h2>
           <p className="text-xl max-w-2xl mx-auto  text-gray-800 dark:text-white">
             Passionate about creating robust and scalable web applications. 
-            Experienced in both frontend and backend technologies.
+            Experienced in both frontend and backend technologies.With Devops experience too.
+            Designed Scallable applications with Docker and Kubernetes
           </p>
         </div>
       </section>
